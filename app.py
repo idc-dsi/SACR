a9c2e047dbcb9e4d1e97a9ddc54598d60b6b462a
GuyMad23 <guy.maduel@runi.post.ac.il>
2023-12-03 20:03:23 +0200

    Azure integration

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, redirect, url_for, render_template, request, session, jsonify, request
+from flask import Flask, redirect, url_for, render_template, request, session, jsonify
 from flask_oauthlib.client import OAuth
 from flask_cors import CORS
 import os
-from azure.storage.blob import BlobServiceClient
+import json
+from azure.storage.blob import BlobServiceClient, BlobClient
+from urllib.parse import unquote
 
 # Flask App Initialization
 app = Flask(__name__)
@@ -33,6 +35,7 @@ storage_account_key = os.getenv('AZURE_STORAGE_ACCOUNT_KEY', 'mYdJ0JI2YBZKj56B1u
 blob_service_client = BlobServiceClient(account_url=f"https://{storage_account_name}.blob.core.windows.net", credential=storage_account_key)
 
 
+
 # Azure Blob Storage Routes
 @app.route('/list-files')
 def list_files():
@@ -41,41 +44,53 @@ def list_files():
         container_client = blob_service_client.get_container_client(container_name)
         blob_list = container_client.list_blobs()
         files = [blob.name for blob in blob_list]
-        links = {file: url_for('read_file', filename=file) for file in files}
-        return render_template('list_files.html', links=links)  # Render a template with the file links
+        return jsonify(files)  # Send a JSON response containing the file names
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/read-file/<filename>')
+@app.route('/read-file/<path:filename>')
 def read_file(filename):
     container_name = "podcasts-coref"
     try:
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=filename)
+        # Decode the URL-encoded filename
+        decoded_filename = unquote(filename)
+        blob_client = BlobClient(account_url=f"https://{storage_account_name}.blob.core.windows.net", 
+                                 container_name=container_name, 
+                                 blob_name=decoded_filename, 
+                                 credential=storage_account_key)
         download_stream = blob_client.download_blob()
-        file_content = download_stream.readall()
-        return render_template('read_file.html', file_content=file_content)  # Render a template with the file content
+        file_content = download_stream.readall().decode('utf-8')
+        return jsonify({"content": file_content})
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({"error": str(e)}), 404
 
-
-@app.route('/write-file/<filename>', methods=['POST'])
-def write_file(filename):
+@app.route('/save-file/<path:filename>', methods=['POST'])
+def save_file(filename):
     container_name = "podcasts-coref"
-    file_content = request.data  # Get file content from the request
     try:
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=filename)
-        blob_client.upload_blob(file_content, overwrite=True)
-        return {"status": "File uploaded successfully."}
+        # Decode the URL-encoded filename
+        decoded_filename = unquote(filename)
+        content = request.data.decode('utf-8')
+        
+        # Logic to save content to the file
+        # Assuming using Azure Blob Storage
+        blob_client = BlobClient(account_url=f"https://{storage_account_name}.blob.core.windows.net", 
+                                 container_name=container_name, 
+                                 blob_name=decoded_filename, 
+                                 credential=storage_account_key)
+        blob_client.upload_blob(content, overwrite=True)
+
+        return jsonify({"message": "File saved successfully"}), 200
     except Exception as e:
-        return {"error": str(e)}
+        return jsonify({"error": str(e)}), 500
+
 
 
 @app.route('/')
 def index():
-    anonymous = request.args.get('anonymous', 0)  # This will be '1' if '?anonymous=1' is in the URL
-    # ... your code to handle anonymous or regular access ...
-    return render_template('index.html', anonymous=anonymous)
-
+    anonymous = request.args.get('anonymous', 0)
+    logged_in = session.get('logged_in', False)
+    return render_template('index.html', anonymous=anonymous,logged_in=logged_in )
 
 @app.route('/login')
 def login():
@@ -84,6 +99,19 @@ def login():
 @app.route('/?anonymous=1')
 def anonymous():
     return render_template('index.html/?anonymous=1', anonymous=True)
+
+@app.route('/api/getFileContent/<path:filename>')
+def get_file_content(filename):
+    # Logic to fetch and return file content
+    # For example, reading from a file system or database
+    try:
+        # Assuming file is stored in a known directory
+        with open(f'path_to_files/{filename}', 'r') as file:
+            content = file.read()
+        return jsonify({"content": content})
+    except IOError:
+        return jsonify({"error": "File not found"}), 404
+
 
 @app.route('/login/authorized')
 def authorized():
@@ -94,7 +122,8 @@ def authorized():
             request.args['error_description']
         )
     session['azure_token'] = (response['access_token'], '')
-    return redirect(url_for('list_files'))
+    # Set a session variable to indicate the user is logged in
+    return redirect(url_for('index', logged_in=True))
  
 @azure.tokengetter
 def get_azure_oauth_token():
